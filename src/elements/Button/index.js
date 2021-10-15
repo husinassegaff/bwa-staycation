cb2c0f955cec160f680b8cb26ea26550b563125f
husinassegaff <husinassegaff15@gmail.com>
2021-10-15 09:46:38 +0700

    fix button component

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import propTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 
-export default function button(props) {
+export default function Button(props) {
     const className = [props.className]
     if(props.isPrimary) className.push("btn-primary")
+    if (props.isLight) className.push("btn-light");
     if(props.isLarge) className.push("btn-lg")
     if(props.isSmall) className.push("btn-sm")
     if(props.isBlock) className.push("btn-block")
-    if(props.hasShadow) className.push("btn-primary")
+    if(props.hasShadow) className.push("btn-shadow")
 
     const onClick = () =>{
-        if(props.onClick) props.onClick()
+        if(props.onClick) props.onClick();
     };
 
     if(props.isDisabled || props.isLoading){
@@ -26,8 +27,8 @@ export default function button(props) {
                 {
                     props.isLoading ?
                         ( <>
-                            <span className="span.spinner-border.spinner-border-sm.mx-5"></span>
-                            <span className="span.sr-only"></span>  
+                            <span className="spinner-border spinner-border-sm mx-5"></span>
+                            <span className="sr-only">Loading...</span>  
                         </> ) : 
                         (props.children)
                 }
@@ -74,17 +75,19 @@ export default function button(props) {
     );
 }
 
-button.propTypes = {
-    type: propTypes.oneOf(["button","link"]),
+Button.propTypes = {
+    type: propTypes.oneOf(["button", "link"]),
     onClick: propTypes.func,
     href: propTypes.string,
     target: propTypes.string,
     className: propTypes.string,
+    isPrimary: propTypes.bool,
+    isLight: propTypes.bool,
+    isExternal: propTypes.bool,
     isDisabled: propTypes.bool,
     isLoading: propTypes.bool,
     isSmall: propTypes.bool,
     isLarge: propTypes.bool,
     isBlock: propTypes.bool,
-    isExternal: propTypes.bool,
     hasShadow: propTypes.bool,
 };
--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -3,33 +3,31 @@ import { render } from "@testing-library/react";
 import { BrowserRouter as Router } from 'react-router-dom'
 import Button from "./index";
 
-// test("Should not allowed click button if isDisabled is present", () =>{
-//     const {container} = render(<Button isDisabled></Button>);
+test("Should not allowed click button if isDisabled is present", () =>{
+    const {container} = render(<Button isDisabled></Button>);
 
-//     expect(container.querySelector("span.disabled")).toBeInTheDocument();
-// });
+    expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
 
-test("Should render loading/spinner", () =>{
-    const {container, getByText} = render(<Button isLoading></Button>);
-
+test("Should render loading/spinner", () => {
+    const { container, getByText } = render(<Button isLoading></Button>);
+  
     expect(getByText(/loading/i)).toBeInTheDocument();
     expect(container.querySelector("span")).toBeInTheDocument();
 });
 
-// test("Should render <a> tag", () =>{
-//     const {container, getByText} = render(<Button type="link" isExternal></Button>)
+test("Should render <a> tag", () =>{
+    const {container} = render(<Button type="link" isExternal></Button>)
 
-//     expect(getByText(/loading/i)).toBeInTheDocument()
-//     expect(container.querySelector("a")).toBeInTheDocument();
-// });
+    expect(container.querySelector("a")).toBeInTheDocument();
+});
 
-// test("Should render <Link> component", () =>{
-//     const {container, getByText} = render(
-//         <Router>
-//             <Button href="" type="link" isExternal></Button>
-//         </Router>
-//     )
+test("Should render <Link> component", () =>{
+    const {container} = render(
+        <Router>
+            <Button href="" type="link" isExternal></Button>
+        </Router>
+    )
 
-//     expect(getByText(/loading/i)).toBeInTheDocument()
-//     expect(container.querySelector("a")).toBeInTheDocument();
-// });
+    expect(container.querySelector("a")).toBeInTheDocument();
+});
